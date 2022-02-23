94408058fa2eeb97d8a66d1f963b73b3824aa763
terryyung <terry.yung@aloha.software>
2022-02-24 00:41:59 +0800

    jest config

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  verbose: true,
 };
