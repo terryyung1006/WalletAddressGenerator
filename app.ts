0245fc57b351a390a709aa26444e15155b3f21d6
terryyung <terry.yung@aloha.software>
2022-02-24 02:46:15 +0800

    err status

--- app.ts
+++ app.ts
@@ -75,8 +75,7 @@ app.get('/hd_segwit_address/:seed_phase/:path', (req: Request, res: Response, ne
     }
     
     
-    let errMsg = HDSegwitAddressGenerator.checkPath(path)
-    errMsg += HDSegwitAddressGenerator.checkSeedPhase(seedPhase)
+    let errMsg = HDSegwitAddressGenerator.checkSeedPhase(seedPhase)
     
     if (errMsg != "") {
         res.status(412).send(errMsg)
@@ -86,13 +85,12 @@ app.get('/hd_segwit_address/:seed_phase/:path', (req: Request, res: Response, ne
     try {
         let address = HDSegwitAddressGenerator.generateSegwitAddress(seedPhase, path)
         if (!address) {
-            res.status(500).send("SegWit address genration failed")
-            return
+            throw "result address empty"
         }
         res.status(200).send(address)
     } catch (e) {
         console.error(e)
-        res.status(500).send("SegWit address genration failed")
+        res.status(400).send("SegWit address genration failed, please check if seed phase and path valid.")
     }
 
 })
@@ -122,11 +120,15 @@ app.get('/hd_segwit_address/:seed_phase/:path', (req: Request, res: Response, ne
  *         required: true
  *         description: array of public keys, length should be same as n, splited by ","
  *         schema:
- *             type: string
- *         example: "02e135e12b4417b41e92e738448cb51581c70f14bf885b0d4056ac2c3cc5c8729c,022015b568fb0f2f792e2e1d230a7f64e8a75b5d4a3ae549b55c3724cdc148b32c,02799dc04a8acf04e793ff0f2c35c20c0388529eb964c565a455f13c07123c9ea2"
+ *             type: array
+ *             items:
+ *               type: string
+ *         example: ["026477115981fe981a6918a6297d9803c4dc04f328f22041bedff886bbc2962e01","02c96db2302d19b43d4c69368babace7854cc84eb9e061cde51cfa77ca4a22b8b9","023e4740d0ba639e28963f3476157b7cf2fb7c6fdf4254f97099cf8670b505ea59"]
  *     responses:
  *       200:
  *         description: OK
+ *       400:
+ *         description: bad request
  *       412:
  *         description: input invalid
  *       500:
@@ -156,13 +158,12 @@ app.get('/p2sh_address/:n/:m/:public_keys', (req: Request, res: Response, next:N
     try {
         let address = MultiSigP2SHAddressGenerator.generateMultiSigP2SHAddress(n, m, publicKeys)
         if (!address) {
-            res.status(500).send("P2SH address genration failed")
-            return
+            throw "result address empty"
         }
         res.status(200).send(address)
     } catch (e) {
         console.error(e)
-        res.status(500).send("P2SH address genration failed")
+        res.status(400).send("P2SH address genration failed, please check if public keys valid.")
     }
 
 })
